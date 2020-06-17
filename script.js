b67d1adf67fd32622d73727e35537bb0965536fa
Cyrus Yip <cyruscmyip1@gmail.com>
2020-06-17 16:28:29 -0700

    animation changes

--- script.js
+++ script.js
@@ -20,25 +20,27 @@ function intro() {
       "+=0.2"
     )
     .from(".period", { y: -100, opacity: 0, ease: "bounce.out" }, "+=0.4")
-    .from(".title p", { opacity: 0, duration: 0.5 }, "-=0.4")
+    .from(".title p", { opacity: 0, duration: 0.5, y: -5 }, "-=0.4")
     .from(".title h1, .title p", {
       x: 30,
       stagger: 0.1,
       ease: "elastic.out(1, 0.75)",
     })
     .from(".links", { y: 20, opacity: 0 }, "<")
-    .from(
-      ".links a",
-      { x: 50, y: 5, opacity: 0, stagger: 0.1, duration: 0.3 },
-      "-=0.2"
-    )
+    .from(".links a", {
+      "padding-top": "20px",
+      "padding-bottom": "20px",
+      opacity: 0,
+      stagger: { amount: 0.2, from: "center" },
+      duration: 0.2,
+    })
     .from(".about", { y: -100, opacity: 0 })
     .from(".about h2", { opacity: 0 })
     .from(".about p span", {
-      x: gsap.utils.random(-200, 200, 10, true),
-      y: gsap.utils.random(-200, 200, 10, true),
+      y: 20,
       opacity: 0,
-      ease: "back.out(1)",
+      ease: "back.out(4)",
+      stagger: { amount: 0.2, from: "center" },
     })
     .from("footer", { opacity: 0, y: 10 }, "<")
     .from("footer>*", { opacity: 0, duration: 0.5 }, "-=0.3");
