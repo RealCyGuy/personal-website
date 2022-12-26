4b1ac6d89bf1ad383d332d80d58e7e72c99b44cd
Cyrus Yip <cyruscmyip1@gmail.com>
2022-12-25 18:06:08 -0800

    add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,15 @@ export default defineNuxtConfig({
   modules: ["@nuxt/content", "@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "@formkit/nuxt"],
   googleFonts: {
     families: { "Hanken+Grotesk": [400, 700, 900] },
+  },
+  app: {
+    head: {
+      link: [
+        {rel: 'apple-touch-icon', sizes: "180x180", href: "/apple-touch-icon.png"},
+        {rel: 'icon', type: "image/png", sizes: "32x32", href: "/favicon-32x32.png"},
+        {rel: 'icon', type: "image/png", sizes: "16x16", href: "/favicon-16x16.png"},
+        {rel: 'manifest', href: "/site.webmanifest"},
+      ]
+    }
   }
 });
