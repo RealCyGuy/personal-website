fbe77b199ba909653e6ba69221826dd1f2b8364b
Cyrus Yip <cyruscmyip1@gmail.com>
2023-11-05 17:27:16 -0800

    improve project links

--- components/Project.vue
+++ components/Project.vue
@@ -1,17 +1,17 @@
 <template>
   <div
-    class="flex sm:gap-3 mb-1 rounded bg-white bg-opacity-0 sm:hover:bg-opacity-5 duration-200 ease-out sm:px-5 py-3 flex-col sm:flex-row"
+    class="flex md:gap-3 mb-1 rounded bg-white bg-opacity-0 md:hover:bg-opacity-5 duration-200 ease-out md:px-5 py-3 flex-col md:flex-row"
     :id="project.title"
   >
     <span class="opacity-90">
       {{ new Date(project.date).getUTCFullYear() }}
     </span>
     <div class="flex justify-between flex-col flex-1">
-      <div class="flex justify-between flex-col sm:flex-row mb-1 sm:mb-0">
+      <div class="flex justify-between flex-col md:flex-row mb-1 md:mb-0">
         <span class="font-bold">
           {{ project.title }}
         </span>
-        <span class="sm:max-w-[50%] sm:text-right break-keep">
+        <span class="md:max-w-[50%] md:text-right break-keep">
           <span
             v-for="(tag, index) in project.tags.sort((a, b) =>
               a.localeCompare(b)
@@ -24,20 +24,27 @@
           </span>
         </span>
       </div>
-      <div class="flex justify-between flex-col sm:flex-row sm:items-end">
+      <div class="flex justify-between flex-col md:flex-row md:items-end">
         <span class="opacity-90">
           <ContentRenderer :value="description" class="prose" />
         </span>
-        <div class="flex gap-1 text-lg -translate-x-1 sm:translate-x-0">
+        <div
+          class="flex gap-1 md:gap-0.5 text-lg -translate-x-1 md:translate-x-0 flex-shrink-0 mt-1 md:mt-0"
+        >
           <NuxtLink
-            v-for="link in links"
+            v-for="(link, index) in links"
             :to="link.url"
-            class="hover:text-pink-300 pl-2"
+            class="hover:text-pink-300 flex-1 px-2 py-2 bg-white bg-opacity-5 md:py-0 md:flex-none flex justify-center items-center gap-2 md:gap-1"
             target="_blank"
             :aria-label="link.label"
             :title="link.label"
+            :class="{
+              'rounded-l-md': index === 0,
+              'rounded-r-md': index === links.length - 1,
+            }"
           >
-            <Icon :name="link.icon" />
+            <Icon class="scale-125 md:scale-100" :name="link.icon" />
+            <span>{{ link.label.slice(8) }}</span>
           </NuxtLink>
         </div>
       </div>
